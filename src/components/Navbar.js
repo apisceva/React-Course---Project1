5434d36d0a02569a6cb35cbef25d28fa5d6864c5
apisceva <aljona.gromova@gmail.com>
2022-08-02 05:54:36 -0400

    working with Navbar and css styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img src={require('../images/react-logo-small.png')} />
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img src={require('../images/react-logo-small.png')} className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
 }
